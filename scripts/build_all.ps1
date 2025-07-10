14cd10482770f19e0aa7e36408d87c1be8ae3380
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:03:20 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Ravenholm_1.0" `
+  -ModName "Ravenholm 1.0" `
+  -ModFolder "RavenholmNCZ" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/ravenholm/RavenholmNCZ-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "e943f21ae1eda8c808a26a0269619858a79e9a6a85ea40bf6fad6f6c84c84505" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
